9f5fb8de20d8997edb94766ff369d4c9f91e58a0
Marcel Groothuis <margro.xbmc@gmail.com>
2011-08-16 20:25:52 +0200

    Increase version number to 1.1.3.107

--- TVServerXBMC/Properties/AssemblyInfo.cs
+++ TVServerXBMC/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.3.106")]
-[assembly: AssemblyFileVersion("1.1.3.106")]
+[assembly: AssemblyVersion("1.1.3.107")]
+[assembly: AssemblyFileVersion("1.1.3.107")]
