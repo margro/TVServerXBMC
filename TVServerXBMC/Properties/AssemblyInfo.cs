95345a0cdc6a0457f096eba9ca503a4161c4c08e
Marcel Groothuis <margro.xbmc@gmail.com>
2013-06-19 20:46:52 +0200

    Update DLLs for MediaPortal 1.4.0

--- TVServerXBMC/Properties/AssemblyInfo.cs
+++ TVServerXBMC/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using MediaPortal.Common.Utils;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.0.122")]
-[assembly: AssemblyFileVersion("1.3.0.122")]
+[assembly: AssemblyVersion("1.4.0.123")]
+[assembly: AssemblyFileVersion("1.4.0.123")]
 
 // Set usage and compatibility information to MediaPortal v1.3.0 beta and above
 [assembly: CompatibleVersion("1.2.200.0", "1.2.200.0")]
