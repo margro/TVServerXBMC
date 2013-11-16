68c77ced476f73f3951126e9e4d8fe770d0037d8
Marcel Groothuis <margro.xbmc@gmail.com>
2013-11-16 22:21:26 +0100

    Create a 1.5.x version of the TVServerXBMC plugin

--- TVServerXBMC/Properties/AssemblyInfo.cs
+++ TVServerXBMC/Properties/AssemblyInfo.cs
@@ -30,10 +30,10 @@ using MediaPortal.Common.Utils;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.0.124")]
-[assembly: AssemblyFileVersion("1.4.0.124")]
+[assembly: AssemblyVersion("1.5.0.124")]
+[assembly: AssemblyFileVersion("1.5.0.124")]
 
 // Set usage and compatibility information to MediaPortal v1.3.0 beta and above
-[assembly: CompatibleVersion("1.4.0.0", "1.2.200.0")]
+[assembly: CompatibleVersion("1.5.0.0", "1.5.0.0")]
 [assembly: UsesSubsystem("TVE")]
 
