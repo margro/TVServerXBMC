86cb161fad31e16ff6ed0a6c776fee59c42a82d1
Marcel Groothuis <margro.xbmc@gmail.com>
2015-10-15 21:34:44 +0200

    Update reference assemblies to v1.12.0

--- TVServerKodi/Properties/AssemblyInfo.cs
+++ TVServerKodi/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using MediaPortal.Common.Utils;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.10.0.128")]
-[assembly: AssemblyFileVersion("1.10.0.128")]
+[assembly: AssemblyVersion("1.12.0.129")]
+[assembly: AssemblyFileVersion("1.12.0.129")]
 
 // Set usage and compatibility information to MediaPortal v1.3.0 beta and above
 [assembly: CompatibleVersion("1.9.100.0", "1.5.0.0")]
