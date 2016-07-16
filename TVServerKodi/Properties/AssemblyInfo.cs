b9ca7c876e8f1411e5da503e4232c607b43e6e74
Marcel Groothuis <margro.xbmc@gmail.com>
2016-07-16 20:37:09 +0200

    Update Changelog

--- TVServerKodi/Properties/AssemblyInfo.cs
+++ TVServerKodi/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using MediaPortal.Common.Utils;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TVServerKodi")]
@@ -15,8 +15,8 @@ using MediaPortal.Common.Utils;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -26,12 +26,12 @@ using MediaPortal.Common.Utils;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.13.100.133")]
-[assembly: AssemblyFileVersion("1.13.100.133")]
+[assembly: AssemblyVersion("1.13.100.134")]
+[assembly: AssemblyFileVersion("1.13.100.134")]
 
 // Set usage and compatibility information to MediaPortal v1.3.0 beta and above
 [assembly: CompatibleVersion("1.9.100.0", "1.5.0.0")]
