031c2919da84cd33273848b2a7682ea5fe27fc27
Marcel Groothuis <margro.xbmc@gmail.com>
2016-07-22 21:05:26 +0200

    Added: IPv6 support

--- TVServerKodi/Properties/AssemblyInfo.cs
+++ TVServerKodi/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using MediaPortal.Common.Utils;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.15.0.134")]
-[assembly: AssemblyFileVersion("1.15.0.134")]
+[assembly: AssemblyVersion("1.15.0.135")]
+[assembly: AssemblyFileVersion("1.15.0.135")]
 
 // Set usage and compatibility information to MediaPortal v1.3.0 beta and above
 [assembly: CompatibleVersion("1.9.100.0", "1.5.0.0")]
