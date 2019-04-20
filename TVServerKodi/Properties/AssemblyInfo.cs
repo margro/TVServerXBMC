acf70e50cee0408d92c917bc397b8c97ac765566
Marcel Groothuis <margro.xbmc@gmail.com>
2019-04-20 15:22:50 +0200

    Update version info for v2.2.0

--- TVServerKodi/Properties/AssemblyInfo.cs
+++ TVServerKodi/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using MediaPortal.Common.Utils;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TVServerKodi")]
-[assembly: AssemblyCopyright("Copyright © Marcel Groothuis 2010-2018")]
+[assembly: AssemblyCopyright("Copyright © Marcel Groothuis 2010-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,8 +29,8 @@ using MediaPortal.Common.Utils;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.20.0.140")]
-[assembly: AssemblyFileVersion("1.20.0.140")]
+[assembly: AssemblyVersion("1.20.0.141")]
+[assembly: AssemblyFileVersion("1.20.0.141")]
 
 // Set usage and compatibility information to for TVServer 1.20 and above
 [assembly: CompatibleVersion("1.20.0.0", "1.5.0.0")]
